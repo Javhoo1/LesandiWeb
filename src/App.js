52fe7c69f8f2acfd9cd3c7381760444d8660bc04
Javhoo1 <javhoo614@gmail.com>
2020-10-16 22:19:23 -0300

    Inicio proyecto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* == Importar Paginas
+
+import {BrowserRouter as Router, Switch, Route,} from 'react-router-dom';
+import Login from "./Pages/Login";
+import Home from "./Pages/Home";
+
+== */
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        /* == Rutas 
+        <Router>
+            <Switch>
+                <Route exact path='/' component={Home}/>
+                <Route path='/Login' component={Login}/>
+            </Switch>
+        </Router>
+
+        */
+       <a>Hooooolaa</a>
+    );
 }
 
 export default App;
